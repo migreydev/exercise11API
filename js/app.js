54c59c5bde70ee840d1fbd8a4dafa5a0adbfbb6c
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2024-10-22 12:30:06 +0200

    añadir producto e implementando borrado

--- js/app.js
+++ js/app.js
@@ -1,34 +1,81 @@
 let api = "http://localhost:8080/api/v1/product";
+let addProduct = "http://localhost:8080/api/v1/product/insert";
 
-fetch(api)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Error en la respuesta");
-    }
-    return response.json();
-  })
-  .then((data) => {
-    for (let i = 0; i < data.length; i++) {
-      let producto = data[i];
-      console.log(producto);
+function getAllProduct() {
+  fetch(api)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error en la respuesta");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        let producto = data[i];
+        console.log(producto);
 
-      let tbody = document.querySelector("tbody");
+        let tbody = document.querySelector("tbody");
 
-      let tr = document.createElement("tr");
-      let tdID = document.createElement("td");
-      let tdName = document.createElement("td");
-      let tdPrice = document.createElement("td");
+        let tr = document.createElement("tr");
+        let tdID = document.createElement("td");
+        let tdName = document.createElement("td");
+        let tdPrice = document.createElement("td");
 
-      tdID.textContent = producto.productId;
-      tdName.textContent = producto.productName;
-      tdPrice.textContent = producto.productPrice;
+        tdID.textContent = producto.productId;
+        tdName.textContent = producto.productName;
+        tdPrice.textContent = producto.productPrice;
 
-      tr.appendChild(tdID);
-      tr.appendChild(tdName);
-      tr.appendChild(tdPrice);
-      tbody.appendChild(tr);
-    }
-  })
-  .catch((error) => {
+        let buttonRemove = document.createElement("button");
+        buttonRemove.textContent = "Delete";
+        buttonRemove.className = "btn btn-danger boton-borrar";
+        buttonRemove.id = producto.productId;
+
+        tr.appendChild(tdID);
+        tr.appendChild(tdName);
+        tr.appendChild(tdPrice);
+        tr.appendChild(buttonRemove);
+        tbody.appendChild(tr);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+let buttonAdd = document.querySelector("#addBoton");
+
+buttonAdd.addEventListener("click", () => {
+  let nameProduct = document.querySelector("#productName").value;
+  let price = document.querySelector("#productPrice").value;
+  let product = null;
+
+  if (nameProduct !== "" && price !== "") {
+    product = {
+      productName: nameProduct,
+      productPrice: price,
+    };
+  }
+
+  fetch(addProduct, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(product),
+  }).catch((error) => {
     console.log(error);
   });
+});
+
+let botonDelete = document.querySelector(".btn.btn-danger.boton-borrar");
+
+if (botonDelete) {
+  botonDelete.addEventListener("click", () => {
+    let idProduct = botonDelete.id;
+    let deleteProduct = `http://localhost:8080/api/v1/product/delete/${idProduct}`;
+
+    fetch(deleteProduct);
+  });
+}
+
+getAllProduct();
